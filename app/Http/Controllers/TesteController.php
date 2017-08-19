89245221abd7189de4b06f386012da1994fd4537
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-08-19 17:59:04 -0300

    Formação Laravel - Inicial

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function index()
+    {
+      return "Teste";
+    }
+
+    public function noauth()
+    {
+      return "Rota sem auth";
+    }
+}
